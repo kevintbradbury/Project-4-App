e2e030f1e930d643420207dd43cc99bb404a010e
kevintbradbury <kevin.t.bradbury@gmail.com>
2016-10-25 10:05:00 -0700

    Added FBCore

--- Mi-Memoir-App/Mi-Memoir-App/ViewController.swift
+++ Mi-Memoir-App/Mi-Memoir-App/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fbButton: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPress(_ sender: AnyObject) {
+        LoginButton
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Mi-Memoir-App/Pods/Target Support Files/Pods-Mi-Memoir-App/Pods-Mi-Memoir-App-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Mi_Memoir_AppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Mi_Memoir_AppVersionString[];
+
