629b7660ef242172332b8aaa0237d727d068a920
kevintbradbury <kevin.t.bradbury@gmail.com>
2016-10-29 19:09:38 -0700

    Set up session key API call
    settled on Walgreens Photo Print API and was able to make successful
    HTTP POST.

--- Mi-Memoir-App/Mi-Memoir-App/MediaPullViewController.swift
+++ Mi-Memoir-App/Mi-Memoir-App/MediaPullViewController.swift
@@ -13,44 +13,85 @@ import UIKit
 
 class MediaPullerView: UIViewController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let printIO = PrintIO(viewController: MediaPullerView(), recipeId: "66119527-67d0-47b0-933f-fabdfc614e4", isInTestMode: true)
+
     }
     
-    var photoSource = MyCustomPhotoSource()
-    var setCustomPhotoSources = printIO()
-    var photoSource = ()
-
+    
+    func getSecretKeyRequest() {
+        
+        let walgreensKeyRequestURL = "https://services-qa.walgreens.com/api/util/mweb5url"
+        
+        let parameters = ["serviceType":"wagS3",
+                          "apiKey":"xIjnSFIkc90LGqIIGeo82Eyj5jOFQ8UQ",
+                          "affId":"photoapi",
+                          "act":"genCredV2",
+                          "view":"genCredV2JSON",
+                          "devinf":"iPhone,6.1",
+                          "appver":"3.1"]
+        
+        let url = URL(string: walgreensKeyRequestURL)!
+        
+        var request = URLRequest.init(url: url)
+        
+//        request.setValue(parameters, forHTTPHeaderField: "Authorization")
+        
+        var error: JSONSerialization.WritingOptions
+        
+        let session = URLSession.shared
+        
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let dataTask = session.dataTask(with: request) {(data, response, error) -> Void in
+            
+            guard let unwrappedData = data else { return }
+            
+            guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: unwrappedData, options: [])) as? NSDictionary else { return }
+            
+            guard let array = (jsonDictionary["return"]) as? NSArray else { return }
+            
+            for i in array {
+                
+                guard let dictionary = i as? NSDictionary else { continue }
+                
+                print(dictionary)
+                
+                guard let secretKeyDictionary = SecretKeyRequest.fromjson(dictionary: dictionary) else {
+                    print("error in guard")
+                    return }
+                
+                //guard let unwrapJSON = try? Data(contentsOf: secretKeyDictionary.accessKeyId) else {return}
+                
+                secretKeyReqstDictionary.append(secretKeyDictionary)
+                
+            }
+            
+        }
+        
+        dataTask.resume()
+        
+    }
+    
+    
+    private let mainOperation = OperationQueue.main
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
 }
 
-class PIODefaultPhotoSource: PIOPhotoSource {
-    
-    func requestItems(for item: PIOPhotoSourceItem!, currentScope: PIOPhotoSourceItem!, count: UInt, page: UInt, withCompletionHandler completionHandler: PIOItemsCompletionHandler!, errorHandler: PIOPhotoSourceErrorHandler!) {
-        
-        title()
-        buttonIconPath()
-        
-        
-    }
-    
-}
 
-class PIODefaultPhotoSourceItem: NSObject, PIOPhotoSourceItem {
 
-    func uniqueIdentifier() -> String! {
-        
-    }
-    func isImageItem() -> Bool {
-        
-    }
-    func fetchImage(in photoSource: PIOPhotoSource!, isThumbnail thumbnail: Bool, withCompletionHandler imageFetchCompletionHandler: ((UIImage?) -> Void)!) {
-        
-    }
-    
-}
+
+
+
+
+
+
+
---
+++ Mi-Memoir-App/Mi-Memoir-App/SecretKeyRequest.swift
@@ -0,0 +1,34 @@
+//
+//  SecretKeyRequest.swift
+//  Mi-Memoir-App
+//
+//  Created by K on 10/29/16.
+//  Copyright © 2016 K. All rights reserved.
+//
+
+import Foundation
+
+var secretKeyReqstDictionary: [SecretKeyRequest] = []
+
+struct SecretKeyRequest {
+    
+    let secretKey: String
+    let accessKeyId: String
+    let sessionId: String
+    let uploadUrl: String
+    let err: String
+    
+    static func fromjson(dictionary: NSDictionary) -> SecretKeyRequest? {
+        guard let secretKey = dictionary["secretKey"] as? String,
+            let accessKeyId = dictionary["accessKeyId"] as? String,
+            let sessionId = dictionary["sessionId"] as? String,
+            let uploadUrl = dictionary["uploadUrl"] as? String,
+            let err = dictionary["err"] as? String else {
+                print("guard failed on SecretKeyRequest Struct")
+                return nil
+        }
+        
+        return SecretKeyRequest(secretKey: secretKey, accessKeyId: accessKeyId, sessionId: sessionId, uploadUrl: uploadUrl, err: err)
+        
+    }
+}
